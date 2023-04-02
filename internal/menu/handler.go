eb45f1b6f3274c68cc9b4f1603f75b93c32107b4
divanov <cool-di@mail.ru>
2023-04-02 16:37:01 +0500

    register page

--- internal/menu/handler.go
+++ internal/menu/handler.go
@@ -3,6 +3,7 @@ package menu
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"table10/internal/handlers"
+	"table10/internal/pages"
 	"table10/internal/pages/cabinet"
 	"table10/internal/pages/tasks"
 	"table10/pkg/logging"
@@ -10,21 +11,37 @@ import (
 
 type handler struct {
 	logger *logging.Logger
+	pages  map[string]pages.Page
 }
 
 func NewHandler(logger *logging.Logger) handlers.Handler {
 	return &handler{
 		logger: logger,
+		pages:  make(map[string]pages.Page),
 	}
 }
 
-func (h *handler) Register(tgbotapi *tgbotapi.Update) (keyBoard *tgbotapi.InlineKeyboardMarkup) {
-	page := tasks.NewPage()
-	switch tgbotapi.CallbackQuery.Data {
-	case "cabinet":
-		page = cabinet.NewPage()
-	case "tasks":
-		page = tasks.NewPage()
+func (h *handler) getPage(pageName string) pages.Page {
+	if page, ok := h.pages[pageName]; ok {
+		return page
 	}
+
+	var newPage pages.Page
+	switch pageName {
+	case "cabinet":
+		newPage = cabinet.NewPage()
+	case "tasks":
+		newPage = tasks.NewPage()
+	default:
+		newPage = tasks.NewPage()
+	}
+
+	h.pages[pageName] = newPage
+	return newPage
+}
+
+func (h *handler) Register(tgbotapi *tgbotapi.Update) (keyBoard *tgbotapi.InlineKeyboardMarkup) {
+	pageName := tgbotapi.CallbackQuery.Data
+	page := h.getPage(pageName)
 	return page.GetKeyboard(tgbotapi)
 }
